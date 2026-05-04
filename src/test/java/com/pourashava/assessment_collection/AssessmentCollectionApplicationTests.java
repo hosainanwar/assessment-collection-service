26f2e875d7f653a682799c315741d863b58a5e11
Anwar Hosain <62494832+hosainanwar@users.noreply.github.com>
2026-05-04 17:02:13 +0600

    initial commit

---
+++ src/test/java/com/pourashava/assessment_collection/AssessmentCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pourashava.assessment_collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
